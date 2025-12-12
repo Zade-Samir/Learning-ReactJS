82261df6ffd677eed85a734b128734d052c4fdbb
Samir Zade <samirzadeofficial43@gmail.com>
2025-12-12 11:16:59 +0530

    useEffect learned

---
+++ 13-useEffect-Hook/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+
+  const [num, setNum] = useState(0)
+
+  useEffect(function() {
+    console.log("use effect is running");
+    
+  })
+
+  return (
+    <div>
+      <h1>{num}</h1>
+      <button onClick={() => {
+        setNum(num + 1)
+      }} >Click</button>
+    </div>
+  )
+}
+
+export default App
